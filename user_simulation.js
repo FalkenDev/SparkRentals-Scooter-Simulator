ebb6a8a9570912907faf2b3806637e0f11014cfe
Joel Funk Persson <joel.funkpersson@gmail.com>
2023-01-03 11:05:24 +0100

    Stop trip when unavailable.

--- user_simulation.js
+++ user_simulation.js
@@ -8,6 +8,7 @@ const rentEndpoint = "/scooters/rent";
 const stopEndpoint = "/scooters/stop";
 const apiKey = process.env.REACT_APP_REST_API_KEY;
 const rentWatchPollRate = 5000;
+const minBalance = 50;
 let token = null;
 
 const admin = {
@@ -56,7 +57,8 @@ async function rentWatch() {
     const scooters = await db.getScootersInUse(process.env.SIMULATION_CITY);
     console.log("scooters in use:", scooters.length);
     scooters.forEach(scooter => {
-        if (scooter.trip && scooter.trip.userId && scooter.speed === 0) {
+        //Stop the trip if speed is 0 or if battery is entering the danger zone
+        if (scooter.trip && scooter.trip.userId && (scooter.speed === 0 || scooter.battery <= 10)) {
             // Scooter has reached destination probably
             console.log("Stopping trip for scooter:", scooter.name);
             stopTrip(scooter._id.toString(), scooter.trip.userId, token);
@@ -70,7 +72,41 @@ async function rentWatch() {
     }
 }
 
+/** Function for stopping all scooters
+  * @return void
+  */
+async function stopAllTrips() {
+    const scooters = await db.getScootersInUse(process.env.SIMULATION_CITY);
+    console.log("scooters in use:", scooters.length);
+    scooters.forEach(async scooter => {
+        if (scooter.trip && scooter.trip.userId) {
+            console.log("Stopping trip for scooter:", scooter.name);
+            stopTrip(scooter._id.toString(), scooter.trip.userId, token);
+        }
+    });
+}
+
+function exitHandler(options, exitCode) {
+    if (options.cleanup) console.log('clean');
+    if (exitCode || exitCode === 0) console.log(exitCode);
+    if (options.exit) process.exit();
+}
+
 async function main() {
+    process.stdin.resume();//so the program will not close instantly
+
+
+    //do something when app is closing
+    process.on('exit', exitHandler.bind(null,{cleanup:true}));
+
+    //catches ctrl+c event
+    process.on('SIGINT', exitHandler.bind(null, {exit:true}));
+
+    // catches "kill pid" (for example: nodemon restart)
+    process.on('SIGUSR1', exitHandler.bind(null, {exit:true}));
+    process.on('SIGUSR2', exitHandler.bind(null, {exit:true}));
+
+    //catches uncaught exceptions
     db.setMongoURI(process.env.DBURI);
     db.connect();
     const users = await db.getAllUsers();
@@ -78,11 +114,12 @@ async function main() {
     console.log("Logging in as admin...");
     token = await adminLogin(admin.email, admin.password);
     console.log("Login successful");
-    //RENT ALL SCOOTERS
+    // Rent as many scooters as possible
     for (let i = 0; i < users.length && i < scooters.length; i++) {
         const user = users[i];
         const scooter = scooters[i];
-        if (scooter.status === "Available" && user.balance > 50 && scooter.owner === process.env.SIMULATION_CITY) {
+        if (scooter.status === "Available" && user.balance > minBalance && scooter.owner === process.env.SIMULATION_CITY) {
+            console.log(process.env.SIMULATION_CITY);
             const response = await rentScooter(scooter._id.toString(), user._id.toString(), token);
         }
     }
